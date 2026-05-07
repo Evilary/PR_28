6b72b60f63a75757242376073ac61b9911a16592
df275 <df275@EVIL>
2026-05-07 19:10:54 +0500

    Проработанная структура и реализованное главное окно. Чернышков

--- TaskManager_Чернышков/MainWindow.xaml.cs
+++ TaskManager_Чернышков/MainWindow.xaml.cs
@@ -1,14 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+﻿using System.Windows;
+using TaskManager_Чернышков.ViewModels;
 namespace TaskManager_Чернышков
 {
     /// <summary>
@@ -16,9 +7,13 @@ namespace TaskManager_Чернышков
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            DataContext = new VM_Pages();
+
         }
     }
 }
