b96ed958fbbea2b703b75e30607477ccc5dd7268
df275 <df275@EVIL>
2026-05-07 21:13:50 +0500

    Разработанный контекст данных для модели задач. Чернышков

---
+++ TaskManager_Чернышков/Context/TasksContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskManager_Чернышков.Classes.Database;
+using TaskManager_Чернышков.Models;
+
+namespace TaskManager_Чернышков.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public TaskContext()
+        {
+            Database.EnsureCreated();
+            Task.Load();
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => 
+            optionsBuilder.UseMySql(Config.connection, Config.version);
+        
+    }
+}
