88aecc312a7867de740a6ed05168b6ff6b6760d7
df275 <df275@EVIL>
2026-05-07 20:16:50 +0500

    Созданный класс Config. Чернышков

---
+++ TaskManager_Чернышков/Classes/Database/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_Чернышков.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=192.168.0.111;" +
+            "uid=root;" +
+            "pwd=;" +
+            "database = TaskManager;";
+
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+
+    }
+}
